ddd6dd3b95cfe2d799caaf9e2cb80d7fc4cb7426
Rodrigo Pestana <58884259+rpestana18@users.noreply.github.com>
2021-02-20 11:08:37 -0300

    Adding pom, .gitignore and project structure

---
+++ src/main/java/com/pestana/kafkaredis/KafkaredisApplication.java
@@ -0,0 +1,13 @@
+package com.pestana.kafkaredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaredisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaredisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pestana/kafkaredis/KafkaredisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pestana.kafkaredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaredisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
